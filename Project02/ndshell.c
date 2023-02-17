2f02c8869e5206319f0fb686749258083ad7ca2d
Joseph Gullo <jgullo@nd.edu>
2023-02-17 14:29:59 -0500

    exit command finished

--- Project02/ndshell.c
+++ Project02/ndshell.c
@@ -12,18 +12,47 @@
 
 // Function declarations
 void exit_shell();
-void start_program(char* myargs[]);
-void wait_for_child();
-void wait_for_specific_child(int child_pid);
+void start_command(char* myargs[]);
+void wait_command();
+void waitfor_command(int child_pid);
 void run_program(char* myargs[]);
 
 int main() {
-    
+    char input[MAX_INPUT_LEN];
+    char *args[MAX_ARGS];
+    int should_run = 1;
+
+    while (should_run) {
+        printf("ndshell> ");
+        fgets(input, MAX_INPUT_LEN, stdin);
+
+        // remove newline character
+        input[strcspn(input, "\n")] = 0;
+
+        // tokenize the input string
+        int i = 0;
+        args[i] = strtok(input, " ");
+
+        while (args[i] != NULL) {
+            i++;
+            args[i] = strtok(NULL, " ");
+        }
+
+        if (args[0] == NULL) {
+            continue;
+        }
+
+        if (strcmp(args[0], "exit") == 0) {
+            exit_shell();
+        }
+    }
+    return 0;
 }
 
 // Exit the shell immediately
 void exit_shell() {
-
+    printf("exiting shell immediately\n");
+    exit(0);
 }
 
 // Starts another program with command line arguments
@@ -36,12 +65,11 @@ void wait_command() {
 
 }
 
-// Wait for a specific child process to exit
 void waitfor_command(int child_pid) {
 
 }
 
 // Start the program and wait for it to complete (start+waitfor)
-void run_program(char* myargs[]) {
+void run_command(char* myargs[]) {
 
 }
