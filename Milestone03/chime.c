1dbb50d19d287a32469295cb053f048cae91e1a4
Joseph Gullo <jgullo@nd.edu>
2023-02-26 18:52:17 -0500

    exit command works correctly

--- Milestone03/chime.c
+++ Milestone03/chime.c
@@ -55,6 +55,17 @@ int main (int argc, char *argv[])
         /* If the command is quit - join any active threads and finish up gracefully */
         if (strcmp(szBuffer, "exit\n") == 0)
         {
+            g_bKeepLooping = 0; // set flag to exit chime threads
+            for(int j=0; j<MAX_THREADS; j++)
+            {
+                if (TheThreads[j].bIsValid == 1)
+                {
+                    printf("Joining Chime %d (Thread %ld)\n", TheThreads[j].nIndex,TheThreads[j].ThreadID);
+                    pthread_join(TheThreads[j].ThreadID, NULL);
+                    printf("Join Complete for Chime %d!\n", TheThreads[j].nIndex);
+                }
+            }
+            printf("Exit chime program ...\n");
             return 0;
         }
         /* If the command is chime, the second argument is the chime number (integer) 
@@ -62,41 +73,46 @@ int main (int argc, char *argv[])
            the thread as needed */
         else if (strncmp(szBuffer, "chime", 5) == 0)
         {
+            // Use sscanf to read in inputs and store them in the correct variables for future use
+            // Sscanf returns the number of fields assigned, which is used for error checking
             parseResult = sscanf(szBuffer, "chime %d %f", &chimeNumber, &chimeInterval);
-            if (parseResult == 2)
+            if (parseResult == 2) //executes if two fields were assigned
             {
+                // executes if the chime number is in the correct range (i.e 0 to 5)
                 if (chimeNumber >= 0 && chimeNumber < MAX_THREADS)
                 {
+                    //Executes if the thread is already running, and adjusts its sleep interval
                     if (TheThreads[chimeNumber].bIsValid == 1)
                     {
-                        /* The thread is already running, adjust its sleep interval */
+                        // Update the chime interval and display message
                         TheThreads[chimeNumber].fChimeInterval = chimeInterval;
                         printf("Adjusting chime %d to have an interval of %f s\n", 
                                chimeNumber, chimeInterval);
                     }
-                    else
+                    else //Executes if the thread is not running, and starts a new thread
                     {
-                        /* The thread is not running, start a new thread */
+                        // Set the Chime fields to the user input
                         TheThreads[chimeNumber].nIndex = chimeNumber;
                         TheThreads[chimeNumber].fChimeInterval = chimeInterval;
                         TheThreads[chimeNumber].bIsValid = 1;
+                        // Create the thread and display message
                         pthread_create(&TheThreads[chimeNumber].ThreadID, NULL, ThreadChime, 
                                         &TheThreads[chimeNumber]);
                         printf("Starting thread %ld for chime %d, interval of %f s\n", 
                                TheThreads[chimeNumber].ThreadID, chimeNumber, chimeInterval);
                     }
                 }
-                else
+                else // Error message if the chime number is not in the correct interval
                 {
                     printf("Cannot adjust chime %d, out of range\n", chimeNumber);
                 }
             }
-            else
+            else // Error message if the fields were incorrectly provided
             {
                 printf("Invalid input format for chime command\n");
             }
         }
-        else
+        else // Error message if the command is not chime or exit
         {
             printf("Unknown command: %s", szBuffer);
         }
